569a3dd1983e538a7695d87735c136e7fdba2800
Idan-dev <cartaidan@gmail.com>
2020-03-29 12:42:46 +0200

    JavaScript réécrit et scripts jquery passés en commentaires supprimés dans les fichiers 'index.html' et 'index.js'

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const socket = io();
-	const input = document.getElementById('m');
-	document.querySelector('form').addEventListener('submit',(event) => {
-		event.preventDefault();
-		socket.emit('chat message', input.value);
-		input.value = '';
-	});
-	socket.on('chat message', (message) => {
-		const li = document.createElement('li');
-		li.textContent = message;
-		document.querySelector('#messages').appendChild(li);
-	});
+const socket = io();
+const input = document.getElementById('m');
+const form = document.querySelector('form');
+
+form.addEventListener('submit',(event) => {
+	event.preventDefault();
+	socket.emit('chat message', input.value);
+	input.value = '';
 });
 
-
-
-/*
-$(function () {
-	var socket = io();
-	$('form').submit(function(e){
-				e.preventDefault(); // prevents page reloading
-				socket.emit('chat message', $('#m').val());
-				$('#m').val('');
-				return false;
-			});
-	socket.on('chat message', function(msg){
-		$('#messages').append($('<li>').text(msg));
-	});
+socket.on('chat message', (message) => {
+	const li = document.createElement('li');
+	li.textContent = message;
+	document.querySelector('#messages').appendChild(li);
 });
-*/
