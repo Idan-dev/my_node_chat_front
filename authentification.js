44d62d771df4d7069a2845ff3dc508ea8e0357dc
Idan-dev <cartaidan@gmail.com>
2020-04-21 15:29:23 +0200

    First element of authentification process: post request

---
+++ authentification.js
@@ -0,0 +1,23 @@
+const pseudo = document.getElementById('pseudo');
+const login = document.getElementById('form');
+let response;
+
+const sendRequest = new XMLHttpRequest;
+
+sendRequest.onreadystatechange = function () {
+	if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
+		response = JSON.parse(this.responseText);
+		console.log(response);
+		window.location.replace('./index.html');
+	}
+};
+
+login.addEventListener('submit', (event) => {
+	event.preventDefault();
+	let userId = pseudo.value
+	sendRequest.open('POST', 'http://localhost:3000/chat')
+	sendRequest.setRequestHeader('Content-type', 'application/json');
+	sendRequest.send(JSON.stringify({name: userId}));
+});
+
+let state = sendRequest.readyState;
