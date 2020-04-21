ab9fe08f3049b2ea92ff386fccbd3a013ff5b566
Idan-dev <cartaidan@gmail.com>
2020-04-21 17:36:11 +0200

    Sign Up and Login management

--- signup.js
+++ signup.js
@@ -5,21 +5,20 @@ let response;
 const sendRequest = new XMLHttpRequest;
 
 sendRequest.onreadystatechange = function () {
-	if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+	if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
 		response = JSON.parse(this.responseText);
-		console.log(response.user.name);
-		//window.location.replace('./index.html');
-	}
+		console.log(response);
+		window.location.replace('./authentification.html');
+	} 
 };
 
 login.addEventListener('submit', (event) => {
 	event.preventDefault();
 	let userId = pseudo.value
-	sendRequest.open('POST', 'http://localhost:3000/chat/login');
+	sendRequest.open('POST', 'http://localhost:3000/chat/signup');
 	sendRequest.setRequestHeader('Content-type', 'application/json');
 	sendRequest.send(JSON.stringify({name: userId}));
-	console.log('Requête envoyée');
-	console.log(JSON.stringify({name: userId }));
+	console.log('Demande envoyée');
 });
 
 let state = sendRequest.readyState;
